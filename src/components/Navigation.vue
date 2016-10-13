b6b0f356d907794917904d9398eb163dd7df0120
Risal Hidayat <risalhidayat88@gmail.com>
2016-10-13 11:28:57 +0700

    intial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="main-menu">
+    <li><router-link to="/">Home</router-link></li>
+    <li>About</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-menu{
+  text-align: left;
+}
+
+.main-menu li{
+  display:inline;
+  text-decoration: none;
+  margin-right: 30px;
+}
+</style>
